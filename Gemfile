ffcb484211f2c3eb738653784acae4d1efe48e6b
Serhii Hordiienko <phrippy2@gmail.com>
2025-07-13 12:57:36 -0500

    Fix telegram formatter method duplication and Ruby version

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.3.3'
+ruby '3.2.3'
 
 gem 'nokogiri', '~> 1.16.6'
 gem 'rexml', '~> 3.3.1'
--- lib/telegram_formatter.rb
+++ lib/telegram_formatter.rb
@@ -44,30 +44,5 @@ module ChatGptMdConverter
 
       remove_blockquote_escaping(output)
     end
-
-    def combine_blockquotes(text)
-      lines = text.split("\n")
-      combined_lines = []
-      blockquote_lines = []
-      in_blockquote = false
-
-      lines.each do |line|
-        if line.start_with?('>')
-          in_blockquote = true
-          blockquote_lines << line[1..].strip
-        else
-          if in_blockquote
-            combined_lines << "<blockquote>#{blockquote_lines.join("\n")}</blockquote>"
-            blockquote_lines = []
-            in_blockquote = false
-          end
-          combined_lines << line
-        end
-      end
-
-      combined_lines << "<blockquote>#{blockquote_lines.join("\n")}</blockquote>" if in_blockquote
-
-      combined_lines.join("\n")
-    end
   end
 end
